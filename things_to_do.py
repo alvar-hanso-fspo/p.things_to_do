6f6804f95b35c3d0aa6ebf90e2734e5a720463b2
mtcdultra <marcostuliobr@gmail.com>
2023-03-22 12:00:03 +0000

    few updates

--- things_to_do.py
+++ things_to_do.py
@@ -1,16 +1,16 @@
 #https://culturedcode.com/things/support/articles/2803573/
 
+
 def to_do_generator():
 
-
-#    project_name = input("Project title: ").replace(" ","_")
-#    to_do_name = input("To-Do name: ").replace(" ","_")
-    #tag_name = input("Tag name: ").replace(" ","_")
-#    initial_number = int(input("First number: "))
-#    final_number = int(input("Final number: "))
+    #    project_name = input("Project title: ").replace(" ","_")
+    #    to_do_name = input("To-Do name: ").replace(" ","_")
+    #    tag_name = input("Tag name: ").replace(" ","_")
+    #    initial_number = int(input("First number: "))
+    #    final_number = int(input("Final number: "))
 
     # **********************
-    project_name = 'JS_NodeJS' # Não deixe espaço em branco
+    project_name = 'JS_NodeJS'  # Não deixe espaço em branco
     to_do_name = 'Aula'
     initial_number = 1
     final_number = 26
@@ -21,10 +21,11 @@ def to_do_generator():
     #tag_name = 'tags='
     to_dos = ""
 
-    result = f'{project_default}{project_name}&{to_do_default}'    
+    result = f'{project_default}{project_name}&{to_do_default}'
 
-    for number in range(initial_number,final_number+1):
+    for number in range(initial_number, final_number + 1):
         to_dos = f'{to_dos}{to_do_name}{str(number)}%0A'
     return result + to_dos
+
 
 print(to_do_generator())
